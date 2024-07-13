ad0a23d4ec036c2259de8f71c3ef849c52bdacdb
Chinmay Chandak <chandakchinmay@gmail.com>
2024-07-13 17:04:26 +0530

    added marketplace smart contract

--- scripts/deployer.js
+++ scripts/deployer.js
@@ -2,6 +2,7 @@ async function main() {
   const Contract = await ethers.getContractFactory("NamasteDuniya");
   const deployedContract = await Contract.deploy();
 
+
   // console.log(deployedContract);
   console.log("Contract deployed to address:", deployedContract.address);
 }
